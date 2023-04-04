bb0bb2353fc74362fdffb482912c9e437ece2035
Santiago <samojica08@gmail.com>
2023-04-04 01:39:58 -0500

    (add) Pantalla Login y componentes
    Se agregaron nuevos componentes, diseño y funciones, tambien se agrego el React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './Pages/Home.jsx'
-import Login from "./Pages/Login.jsx";
-import Usuario from "./Pages/Usuario.jsx";
-import Cases from "./Pages/Cases.jsx";
+import Home from './Components/Home/Home.jsx';
+import Login from './Components/Login/Login.jsx';
+import Usuario from './Components/Usuario/Usuario.jsx';
+import Cases from "./Components/Cases/Cases.jsx";
 
 
 function App() {
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Routes>
-        <Route exact path="/" component={Login} />
-        <Route path="/Home" Component={Home} />
-        <Route path="/Usuario" component={Usuario} />
-        <Route path="/Cases" component={Cases} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route exact path="/" Component={Login} />
+          <Route path="/Home" Component={Home} />
+          <Route path="/:Usuario" component={Usuario} />
+          <Route path="/Cases" component={Cases} />
+        </Routes>
+      </Router>
     </div>
   );
 }
