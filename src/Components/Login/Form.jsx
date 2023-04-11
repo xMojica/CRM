76bf5ad84efc534d90b8d26ac68c696eb4214f6c
Santiago <samojica08@gmail.com>
2023-04-11 17:11:17 -0500

    (FIX) Se corrige el placeolder de document

--- src/Components/Login/Form.jsx
+++ src/Components/Login/Form.jsx
@@ -195,7 +195,7 @@ function Form(props) {
             <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
           </svg>
           <input
-            placeholder="Password"
+            placeholder="New password"
             className="input-field"
             type="password"
             onChange={contraseña}
